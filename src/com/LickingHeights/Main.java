08afa59b7a8ba3da118509cdf829841b897b0792
Muna Chimoriya <muna.chimoriya.2022student.lhschools.org>
2020-04-15 14:53:02 -0400

    Logic loop created

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -14,5 +14,44 @@ public class Main {
         System.out.println(" if you guess higher than my number, I will let you know that you guessed too high.");
         System.out.println("Lets play then, shall we?");
 
+        Scanner keyboard;
+        NumberGuessingGame();
+        int counter;
+        int guess = 0;
+    }
+
+    public static void NumberGuessingGame() {
+        int secretNumber;
+        String answer;
+
+        Scanner keyboard = new Scanner(System.in);
+        int guess;
+
+        do {
+            secretNumber = ((int) (Math.random() * 20) + 1);
+            int counter = 0;
+
+
+            do {
+                System.out.println("Guess a number between 1 and 20");
+                guess = keyboard.nextInt();
+                counter++;
+                if (guess > secretNumber) {
+
+                    System.out.println("You have guessed too high");
+
+
+                } else if (guess < secretNumber) {
+                    System.out.println("You have guessed too low");
+
+
+                } else {
+                    System.out.println("Congratulations you have guess the correct answer in only " + counter + " tries ;)");
+                }
+            } while (guess != secretNumber);
+
+
+
+
     }
 }
