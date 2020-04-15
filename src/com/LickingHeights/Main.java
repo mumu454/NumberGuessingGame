1a222b958d1b369a381170e117ddb3e3760a0383
Muna Chimoriya <muna.chimoriya.2022student.lhschools.org>
2020-04-15 14:28:38 -0400

    Game rules implemented

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,18 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(" Welcome to the Number Guessing game!");
+        System.out.println("The rules are easy-peasy-lemon-squeezy.");
+        System.out.println(" First, I will pick a number and you have to try to guess it.");
+        System.out.println(" The number that I pick will between 1 and  20.");
+        System.out.println(" If you guess lower than my number, I will let you know that you guessed too low, ");
+        System.out.println(" if you guess higher than my number, I will let you know that you guessed too high.");
+        System.out.println("Lets play then, shall we?");
+
     }
 }
